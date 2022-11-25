9495343f49ffe8fc6346f41440d19a43fae999d9
techsyhub <techsyhub@gmail.com>
2022-11-25 02:58:27 -0800

    chat schema

--- server/model/chat.js
+++ server/model/chat.js
@@ -1,1 +1,31 @@
 const mongoose = require("mongoose")
+
+const chatSchema = mongoose.Schema({
+    chatName:{
+        type:String,
+        trim:true
+    },
+    isGroupChat:{
+        type:Boolean,
+        default:false
+    },
+    user:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ],
+    latestMessage:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Message"
+    },
+    groupAdmin:{
+        type:mongoose.Schema.Types.ObjectId
+    }  
+    },
+    {
+        timestamps:true
+    }
+)
+
+module.exports= mongoose.model("Chat", chatSchema)
